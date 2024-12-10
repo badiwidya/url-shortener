256f9d6c91d247658a5fdf82d32f311f4a09a808
badiwidya <badiwidya@users.noreply.github.com>
2024-12-11 01:30:57 +0700

    Change endpoint

--- routes/urlRoute.js
+++ routes/urlRoute.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { shortUrl, getUrl } from "../controllers/urlController.js";
 const router = Router();
 
-router.post("/short-req", shortUrl);
+router.post("/url", shortUrl);
 router.get("/:url", getUrl);
 
 export default router;
